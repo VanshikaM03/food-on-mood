bd420a7f2563aff648704c207c7fec446a058bcc
Vanshika <vanshikamittal2020@gmail.com>
2023-07-13 16:38:54 +0530

    chatbot

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
       <main>
         <Meals />
       </main>
+      <script
+        src="//code.tidio.co/4jffgtjik1wc6evdhqba0kyzqplxr5jc.js"
+        async
+      ></script>
     </Fragment>
   );
 }
